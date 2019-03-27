bf809b035d9575443fd2da3c66edf68feff8ccd3
nelsontrangithub <nelsontran@icloud.com>
2019-03-26 21:12:47 -0700

    added Index view

---
+++ ExpenseTracker/Controllers/ExpenseController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ExpenseTracker.Data;
+using ExpenseTracker.Models;
+using ExpenseTracker.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExpenseTracker.Controllers
+{
+    public class ExpenseController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ExpenseController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        // GET: Expense
+        public ActionResult Index()
+        {
+            if (User.Identity.Name == null)
+            {
+                return Redirect("/Identity/Account/Login");
+            }
+            List<Expense> expenses = new List<Expense>();
+            ExpenseRepo expRepo = new ExpenseRepo(_db);
+            expenses = expRepo.GetAllExpenses().ToList();
+            return View(expenses);
+        }
+
+        // GET: Expense/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Expense/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Expense/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Expense/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Expense/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Expense/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Expense/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
