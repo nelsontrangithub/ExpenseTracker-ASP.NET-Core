fa57476a19c6cfc7c775df5ee5ed4422ea6d03c5
nelsontrangithub <nelsontran@icloud.com>
2019-03-26 18:22:48 -0700

    created Expense model

---
+++ ExpenseTracker/Models/Expenses.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpenseTracker.Models
+{
+    public class Expenses
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string ExpenseName { get; set; }
+        [Required]
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(10, 2)")]
+        public decimal Amount { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}", ApplyFormatInEditMode = true)]
+        [Required]
+        public DateTime ExpenseDate { get; set; } = DateTime.Now;
+        [Required]
+        public string Category { get; set; }
+    }
+}
